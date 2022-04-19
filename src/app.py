f717e2cfe4aaaa745315a7dba63dc950d70f8ac5
krallice <sroz9960@gmail.com>
2022-04-19 12:23:43 +0000

    app main loop every 10s

--- src/app.py
+++ src/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import re
+import time
 import requests
 from bs4 import BeautifulSoup
 
@@ -14,20 +15,24 @@ SEARCHRANGES = [
 
 def do_main():
 
-    # Retrieve URL:
-    response = requests.get(TARGETURL)
-    if response.status_code != 200:
-        return
-    
-    # Parse the DOM:
-    response_soup = BeautifulSoup(response.text, 'html.parser')
-    gripper_options = response_soup.findAll('select', attrs = {'id': 'SingleOptionSelector-0'})[0].findAll('option')
-    # gripper_options = [response_soup.findAll('option') for i in response_soup.findAll('select', attrs = {'id': 'SingleOptionSelector-0'})]
-    
-    for gripper in gripper_options:
-        for searchrange in SEARCHRANGES:
-            if (int(gripper['value'].split()[0]) > searchrange['LOWERBOUND'] and int(gripper['value'].split()[0]) < searchrange['UPPERBOUND']):
-                print(gripper['value'])
+    while True:
+
+        # Retrieve URL:
+        response = requests.get(TARGETURL)
+        if response.status_code != 200:
+            return
+        
+        # Parse the DOM:
+        response_soup = BeautifulSoup(response.text, 'html.parser')
+        gripper_options = response_soup.findAll('select', attrs = {'id': 'SingleOptionSelector-0'})[0].findAll('option')
+        # gripper_options = [response_soup.findAll('option') for i in response_soup.findAll('select', attrs = {'id': 'SingleOptionSelector-0'})]
+        
+        for gripper in gripper_options:
+            for searchrange in SEARCHRANGES:
+                if (int(gripper['value'].split()[0]) > searchrange['LOWERBOUND'] and int(gripper['value'].split()[0]) < searchrange['UPPERBOUND']):
+                    print(gripper['value'])
+
+        time.sleep(10)
 
 if __name__ == '__main__':
     do_main()
